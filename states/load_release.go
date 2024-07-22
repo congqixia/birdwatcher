4fb47373407d836e49ccd1f4290992277aa9c60d
Congqi Xia <congqi.xia@zilliz.com>
2024-07-22 20:37:55 +0800

    Add load release collection command
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

---
+++ states/load_release.go
@@ -0,0 +1,166 @@
+package states
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/cockroachdb/errors"
+	"github.com/milvus-io/birdwatcher/framework"
+	"github.com/milvus-io/birdwatcher/models"
+	commonpbv2 "github.com/milvus-io/birdwatcher/proto/v2.2/commonpb"
+	datapbv2 "github.com/milvus-io/birdwatcher/proto/v2.2/datapb"
+	"github.com/milvus-io/birdwatcher/proto/v2.2/indexpb"
+	"github.com/milvus-io/birdwatcher/proto/v2.2/milvuspb"
+	querypbv2 "github.com/milvus-io/birdwatcher/proto/v2.2/querypb"
+	rootcoordpbv2 "github.com/milvus-io/birdwatcher/proto/v2.2/rootcoordpb"
+	"github.com/milvus-io/birdwatcher/states/etcd/common"
+	"github.com/samber/lo"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type LoadCollectionParam struct {
+	framework.ParamBase `use:"load collection" desc:"load collection"`
+	DB                  int64 `name:"db" default:"0" desc:"db id"`
+	Collection          int64 `name:"collection" default:"0" desc:"collection ID"`
+	ReplicaNum          int64 `name:"replica" default:"1" desc:"collection"`
+}
+
+func (s *InstanceState) LoadCollectionCommand(ctx context.Context, p *LoadCollectionParam) error {
+	sessions, err := common.ListSessions(s.client, s.basePath)
+	if err != nil {
+		return err
+	}
+	rcSession, found := lo.Find(sessions, func(session *models.Session) bool {
+		return session.ServerName == "rootcoord"
+	})
+
+	if !found {
+		return errors.New("rootcoord session not found")
+	}
+
+	opts := []grpc.DialOption{
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithBlock(),
+	}
+
+	conn, err := grpc.DialContext(ctx, rcSession.Address, opts...)
+	if err != nil {
+		return errors.New("find to connect to rootcoord")
+	}
+	rootcoord := rootcoordpbv2.NewRootCoordClient(conn)
+
+	resp, err := rootcoord.DescribeCollectionInternal(ctx, &milvuspb.DescribeCollectionRequest{
+		Base: &commonpbv2.MsgBase{
+			TargetID: rcSession.ServerID,
+			MsgType:  commonpbv2.MsgType_DescribeCollection,
+		},
+		CollectionID: p.Collection,
+	})
+	if err != nil {
+		return err
+	}
+
+	schema := resp.GetSchema()
+
+	dcSession, found := lo.Find(sessions, func(session *models.Session) bool {
+		return session.ServerName == "datacoord"
+	})
+	if !found {
+		return errors.New("rootcoord session not found")
+	}
+	conn, err = grpc.DialContext(ctx, dcSession.Address, opts...)
+	if err != nil {
+		return errors.New("find to connect to rootcoord")
+	}
+	datacoord := datapbv2.NewDataCoordClient(conn)
+
+	idxResp, err := datacoord.DescribeIndex(ctx, &indexpb.DescribeIndexRequest{
+		CollectionID: p.Collection,
+	})
+	if err != nil {
+		return err
+	}
+
+	fieldIndexIDs := make(map[int64]int64)
+	for _, index := range idxResp.IndexInfos {
+		fieldIndexIDs[index.FieldID] = index.IndexID
+	}
+
+	qcSession, found := lo.Find(sessions, func(session *models.Session) bool {
+		return session.ServerName == "querycoord"
+	})
+	if !found {
+		return errors.New("querycoord session not found")
+	}
+	conn, err = grpc.DialContext(ctx, qcSession.Address, opts...)
+	if err != nil {
+		return errors.New("find to connect to querycoord")
+	}
+	querycoord := querypbv2.NewQueryCoordClient(conn)
+
+	request := &querypbv2.LoadCollectionRequest{
+		Base: &commonpbv2.MsgBase{
+			TargetID: qcSession.ServerID,
+			MsgType:  commonpbv2.MsgType_LoadCollection,
+		},
+		DbID:           0,
+		CollectionID:   p.Collection,
+		Schema:         schema,
+		ReplicaNumber:  int32(p.ReplicaNum),
+		FieldIndexID:   fieldIndexIDs,
+		Refresh:        false,
+		ResourceGroups: nil,
+	}
+	loadCollectionResp, err := querycoord.LoadCollection(ctx, request)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("load collection: %s\n", loadCollectionResp.GetErrorCode().String())
+
+	return nil
+}
+
+type ReleaseCollectionParam struct {
+	framework.ParamBase `use:"release collection" desc:"release collection"`
+	DB                  int64 `name:"db" default:"0" desc:"db id"`
+	Collection          int64 `name:"collection" default:"0" desc:"collection ID"`
+}
+
+func (s *InstanceState) ReleaseCollectionCommand(ctx context.Context, p *ReleaseCollectionParam) error {
+	sessions, err := common.ListSessions(s.client, s.basePath)
+	if err != nil {
+		return err
+	}
+	qcSession, found := lo.Find(sessions, func(session *models.Session) bool {
+		return session.ServerName == "querycoord"
+	})
+	if !found {
+		return errors.New("querycoord session not found")
+	}
+	opts := []grpc.DialOption{
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithBlock(),
+	}
+	conn, err := grpc.DialContext(ctx, qcSession.Address, opts...)
+	if err != nil {
+		return errors.New("find to connect to querycoord")
+	}
+	querycoord := querypbv2.NewQueryCoordClient(conn)
+
+	req := &querypbv2.ReleaseCollectionRequest{
+		Base: &commonpbv2.MsgBase{
+			TargetID: qcSession.ServerID,
+		},
+		CollectionID: p.Collection,
+	}
+
+	resp, err := querycoord.ReleaseCollection(ctx, req)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("load collection: %s\n", resp.GetErrorCode().String())
+	return nil
+}
