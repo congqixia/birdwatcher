49d80d6702ddef87892578e42e2762c0850c668f
Congqi Xia <congqi.xia@zilliz.com>
2025-07-18 15:55:49 +0800

    add rc rpc
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

--- states/mgrpc/rootcoord.go
+++ states/mgrpc/rootcoord.go
@@ -4,10 +4,13 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/cockroachdb/errors"
 	"google.golang.org/grpc"
 
 	"github.com/milvus-io/birdwatcher/framework"
 	"github.com/milvus-io/birdwatcher/models"
+	"github.com/milvus-io/milvus-proto/go-api/v2/commonpb"
+	"github.com/milvus-io/milvus-proto/go-api/v2/milvuspb"
 	"github.com/milvus-io/milvus/pkg/v2/proto/rootcoordpb"
 )
 
@@ -32,6 +35,82 @@ type TestParam struct {
 
 func (s *rootCoordState) TestCommand(ctx context.Context, p *TestParam) error {
 	fmt.Println("rootcoord test")
+	return nil
+}
+
+type DescribeCollectionParam struct {
+	framework.ParamBase `use:"describe collection" desc:"describe collection by collectionID"`
+	CollectionID        int64 `name:"collectionID" default:"0" desc:"collection id to compact"`
+}
+
+func (s *rootCoordState) DescribeCollectionCommand(ctx context.Context, p *DescribeCollectionParam) error {
+	resp, err := s.client.DescribeCollection(ctx, &milvuspb.DescribeCollectionRequest{
+		Base: &commonpb.MsgBase{
+			MsgType: commonpb.MsgType_DescribeCollection,
+		},
+		CollectionID: p.CollectionID,
+	})
+	if err != nil {
+		return errors.Wrapf(err, "Describe collection with collectionID:%d", p.CollectionID)
+	}
+	fmt.Printf("CollectionID: %d, Name: %s, Schema: %s\n", resp.CollectionID, resp.CollectionName, resp.Schema.String())
+	return nil
+}
+
+type AlterCollectionParam struct {
+	framework.ParamBase `use:"alter collection" desc:"alter collection by collectionID"`
+	DbName              string `name:"dbName" default:"" desc:"database name to alter"`
+	CollectionID        int64  `name:"collectionID" default:"0" desc:"collection id to alter"`
+	CollectionName      string `name:"collectionName" default:"" desc:"collection name to alter"`
+	Key                 string `name:"key" default:""`
+	Value               string `name:"value" default:""`
+}
+
+func (s *rootCoordState) AlterCollectionCommand(ctx context.Context, p *AlterCollectionParam) error {
+	resp, err := s.client.AlterCollection(ctx, &milvuspb.AlterCollectionRequest{
+		Base: &commonpb.MsgBase{
+			MsgType: commonpb.MsgType_AlterCollection,
+		},
+		DbName:         p.DbName,
+		CollectionID:   p.CollectionID,
+		CollectionName: p.CollectionName,
+		Properties: []*commonpb.KeyValuePair{
+			{
+				Key:   p.Key,
+				Value: p.Value,
+			},
+		},
+	})
+	if err != nil {
+		return errors.Newf("alter collection fail: %s", err.Error())
+	}
+	fmt.Printf("AlterCollection resp: %v\n", resp)
+	return nil
+}
+
+type DeleteCollectionPropertyParam struct {
+	framework.ParamBase `use:"delete collection-property" desc:"alter collection by collectionID"`
+	DbName              string `name:"dbName" default:"" desc:"database name to alter"`
+	CollectionID        int64  `name:"collectionID" default:"0" desc:"collection id to alter"`
+	CollectionName      string `name:"collectionName" default:"" desc:"collection name to alter"`
+	Key                 string `name:"key" default:""`
+	// Value               string `name:"value" default:""`
+}
+
+func (s *rootCoordState) DeleteCollectionPropertyCommand(ctx context.Context, p *DeleteCollectionPropertyParam) error {
+	resp, err := s.client.AlterCollection(ctx, &milvuspb.AlterCollectionRequest{
+		Base: &commonpb.MsgBase{
+			MsgType: commonpb.MsgType_AlterCollection,
+		},
+		DbName:         p.DbName,
+		CollectionID:   p.CollectionID,
+		CollectionName: p.CollectionName,
+		DeleteKeys:     []string{p.Key},
+	})
+	if err != nil {
+		return errors.Newf("alter collection fail: %s", err.Error())
+	}
+	fmt.Printf("AlterCollection resp: %v\n", resp)
 	return nil
 }
 
