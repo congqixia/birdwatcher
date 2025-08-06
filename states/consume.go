3c7ce59f7412c14d25c3dc923bb05b2255a63e69
Congqi Xia <congqi.xia@zilliz.com>
2025-08-06 19:43:06 +0800

    Ignore parse error
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

--- states/consume.go
+++ states/consume.go
@@ -106,10 +106,10 @@ func (s *InstanceState) ConsumeCommand(ctx context.Context, p *ConsumeParam) err
 			// fmt.Printf("%s ", msgType)
 			switch msgType {
 			case commonpb.MsgType_Insert, commonpb.MsgType_Delete:
-				v, err := ParseInsertMsg(header.GetBase().GetMsgType(), msg.Payload())
-				if err != nil {
-					fmt.Println(err.Error())
-				}
+				v, _ := ParseInsertMsg(header.GetBase().GetMsgType(), msg.Payload())
+				// if err != nil {
+				// 	fmt.Println(err.Error())
+				// }
 				if v != nil && (p.ShardName == "" || v.GetShardName() == p.ShardName) {
 					fmt.Println("timestamp", v.Base.GetTimestamp())
 					for _, fieldData := range v.GetFieldsData() {
